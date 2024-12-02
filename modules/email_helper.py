03befd4418bae374791555ca5f4520a7fc774813
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-12-02 12:31:06 -0500

    Add message topic to the email subject and body. Remove redundant list conversion of DataFrame object.

--- modules/email_helper.py
+++ modules/email_helper.py
@@ -5,17 +5,33 @@ import os
 import modules.config as config
 
 
-def prepare_email(sender, recipient, message):
+def get_email_subject(topic):
+    subject = f"[{config.APP_TITLE}] "
+
+    match topic:
+        case "Job Inquiries":
+            subject += "New Job Inquiry Received"
+        case "Project Proposals":
+            subject += "New Project Proposal Received"
+        case _:
+            subject += "New Message Received"
+
+    return subject
+
+
+def prepare_email(sender, recipient, topic, message):
     email_message = email.message.Message()
     email_message.add_header('from', f"{config.APP_TITLE} <{sender}>")
     email_message.add_header('to', recipient)
-    email_message.add_header('subject', f"[{config.APP_TITLE}] New Message Received")
-    email_message.set_payload(f"Sender:\n{sender}\n\nMessage:\n{message}")
+    email_message.add_header('subject', get_email_subject(topic))
+    email_message.set_payload(
+        f"Sender:\n{sender}\n\nTopic:\n{topic}\n\nMessage:\n{message}"
+    )
 
     return email_message.as_string()
 
 
-def send_email(sender, message):
+def send_email(sender, topic, message):
     host = config.APP_EMAIL_HOST
     port = config.APP_EMAIL_PORT
     username = os.getenv("APP_EMAIL_USER")
@@ -23,7 +39,7 @@ def send_email(sender, message):
     context = ssl.create_default_context()
     recipient = os.getenv("APP_EMAIL_INBOX")
 
-    email_message = prepare_email(sender, recipient, message)
+    email_message = prepare_email(sender, recipient, topic, message)
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
--- pages/contact.py
+++ pages/contact.py
@@ -13,18 +13,17 @@ components.page_navigation()
 st.title("Contact")
 
 data = pandas.read_csv("data/topics.csv")
-topics = data["topic"].to_list()
 
 with st.form(key="contact_form"):
     sender = st.text_input("Your email address:")
-    topic = st.selectbox("What topic do you want to discuss?", topics)
+    topic = st.selectbox("What topic do you want to discuss?", data["topic"])
     message = st.text_area("Your message:")
     submit = st.form_submit_button("Send Message")
 
 if submit:
     status = st.info("Sending...")
     try:
-        email_helper.send_email(sender, message)
+        email_helper.send_email(sender, topic, message)
         status.info("Your message was sent successfully.")
     except:
         status.error("Your message could not be sent.")
